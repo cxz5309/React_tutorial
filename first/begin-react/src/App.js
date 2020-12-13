7067e88c4717798400c859e029b7529ff1c17f2e
cxz5309 <42149645+cxz5309@users.noreply.github.com>
2020-12-13 19:06:57 +0900

    리액트 튜토리얼1

---
+++ first/begin-react/src/App.js
@@ -0,0 +1,85 @@
+import React, {useRef, useState} from 'react';
+import CreateUser from './CreateUser';
+import UserList from './UserList';
+
+function App(){
+    const[inputs, setInputs] = useState({
+        username: '',
+        email: '',
+    });
+    const {username, email} = inputs;
+
+    
+    const onChange = (e) =>{
+        const {name, value} = e.target;
+        setInputs({
+            ...inputs,
+            [name]:value
+        });
+    };
+
+    const [users, setUsers] = useState([
+        {
+            id:1,
+            username:'velopert',
+            email:'cxz5309@naver.com',
+            active : true
+        },   
+        {
+            id:2,
+            username:'tester',
+            email:'cxz5309@gmail.com',
+            active : false
+        },   
+        {
+            id:3,
+            username:'liz',
+            email:'cxz5309@daum.com',
+            active : false
+        }
+    ]);
+
+    const nextId = useRef(4);
+
+    const onCreate =()=>{
+
+        const user = {
+            id:nextId.current,
+            username,
+            email
+        }
+
+        setUsers(users.concat(user));
+
+        setInputs({
+            username: '',
+            email: ''
+        })
+        nextId.current += 1;
+    }
+
+    const onRemove = id =>{
+        setUsers(users.filter(user=>user.id !== id));
+    }
+
+    const onToggle = id =>{
+        setUsers(users.map(
+            user => user.id ===id ? {...user, active : !user.active}
+            : user
+        ))
+    }
+
+    return (
+        <>
+            <CreateUser 
+                username={username} 
+                email={email} 
+                onChange={onChange} 
+                onCreate={onCreate}
+            />
+            <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
+        </>
+    );
+}
+
+export default App;
