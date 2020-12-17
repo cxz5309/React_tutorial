d889db9903fcc0ba1015fee3fffb0051fdf22a6d
cxz5309 <42149645+cxz5309@users.noreply.github.com>
2020-12-17 10:49:20 +0900

    Context Api, Immer 등

--- first/begin-react/src/App.js
+++ first/begin-react/src/App.js
@@ -1,6 +1,8 @@
-import React, {useRef, useReducer, useMemo, useCallback} from 'react';
+import React, {useRef, useReducer, useMemo, useCallback, createContext} from 'react';
+import produce from 'immer';
 import CreateUser from './CreateUser';
 import UserList from './UserList';
+import useInputs from './useInputs';
 
 function countActiveUsers(users){
     console.log("활성 사용자 수를 세는중...");
@@ -8,10 +10,7 @@ function countActiveUsers(users){
 }
 
 const initialState = {
-    inputs:{
-        username:'',
-        email:'',
-    },
+  
     users:[
         {
             id:1,
@@ -36,53 +35,54 @@ const initialState = {
 
 function reducer(state, action){
     switch(action.type){
-        case 'CHANGE_INPUT':
-            return {
-                ...state, 
-                inputs: {
-                    ...state.inputs,
-                    [action.name]: action.value
-                }
-            }
         case 'CREATE_USER':
-            return {
-                inputs: initialState.inputs,
-                users: state.users.concat(action.user)
-            }
+            // return {
+            //     inputs: initialState.inputs,
+            //     users: state.users.concat(action.user)
+            // }
+            return produce(state, draft =>{
+                draft.users.push(action.user);
+            })
         case 'TOGGLE_USER':
-            return{
-                ...state,
-                users: state.users.map(user=>
-                    user.id === action.id
-                    ? {...user, active: !user.active}
-                    : user
-                )
-            }
+            // return{
+            //     ...state,
+            //     users: state.users.map(user=>
+            //         user.id === action.id
+            //         ? {...user, active: !user.active}
+            //         : user
+            //     )
+            // }
+            return produce(state, draft=>{
+                const user = draft.users.find(user=>user.id === action.id);
+                user.active = !user.active;
+            });
         case 'REMOVE_USER':
-            return {
-                ...state,
-                users: state.users.filter(user => user.id !== action.id)
-            }
+            // return {
+            //     ...state,
+            //     users: state.users.filter(user => user.id !== action.id)
+            // }
+            return produce(state, draft=>{
+                const index = draft.users.findIndex(user => user.id === action.id);
+                draft.users.splice(index,1);
+            });
         default:
             throw new Error('Unhandled action');
     }
 }
 
+export const UserDispatch = createContext(null);
+
 function App(){
     
     const [state, dispatch] = useReducer(reducer, initialState)
     const {users} = state;
-    const {username, email} = state.inputs;
     const nextId = useRef(4);
+    const [form, onChange, reset] = useInputs({
+        username: '',
+        email: ''
+    });
+    const {username, email} = form;
 
-
-    const onChange = useCallback(e=>{
-        const{name, value} = e.target;
-        dispatch({
-            type: 'CHANGE_INPUT',
-            name, value
-        })
-    }, [])
 
     const onCreate = useCallback(()=>{
         dispatch({
@@ -94,36 +94,23 @@ function App(){
             }
         });
         nextId.current += 1;
+        reset();
     }, [username, email])
-
-    const onToggle = useCallback(id=>{
-        dispatch({
-            type: 'TOGGLE_USER',
-            id
-        });
-    },[]);
-
-    const onRemove = useCallback(id =>{
-        dispatch({
-            type:'REMOVE_USER',
-            id
-        });
-    }, []);
 
     const count = useMemo(()=> countActiveUsers(users), [users]);
     return (
         <>
-            <CreateUser 
-                username={username}
-                email = {email}
-                onChange = {onChange}
-                onCreate = {onCreate}
-            />
-            <UserList users={users}
-                onToggle = {onToggle}
-                onRemove = {onRemove}
-            />
-            <div> 활성 사용자 수: {count}</div>
+            <UserDispatch.Provider value={dispatch}>
+                <CreateUser 
+                    username={username}
+                    email = {email}
+                    onChange = {onChange}
+                    onCreate = {onCreate}
+                />
+                <UserList users={users}
+                />
+                <div> 활성 사용자 수: {count}</div>
+            </UserDispatch.Provider>
         </>
     );
 }
--- first/begin-react/src/UserList.js
+++ first/begin-react/src/UserList.js
@@ -1,7 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useContext } from 'react';
+import {UserDispatch} from './App';
 
-const User = React.memo(function User({user, onRemove, onToggle}){
+const User = React.memo(function User({user}){
     const {username, email, id, active} = user;
+    const dispatch = useContext(UserDispatch);
 
     useEffect(()=>{
         console.log(user);
@@ -17,18 +19,24 @@ const User = React.memo(function User({user, onRemove, onToggle}){
                     color: active? 'green': 'black',
                     cursor: 'pointer'
                 }}
-                onClick={()=>onToggle(id)}
+                onClick={()=>dispatch({
+                    type: 'TOGGLE_USER',
+                    id
+                })}
             >
                 {username}
             </b>
             &nbsp;
             <br/><span>{email}</span>
-           <button onClick={()=>onRemove(id)}>삭제</button>
+           <button onClick={()=>dispatch({
+               type: "REMOVE_USER",
+               id
+           })}>삭제</button>
         </div>
     );
 });
 
-function UserList({users, onRemove, onToggle}){
+function UserList({users}){
     return (
         <div>
             {
@@ -37,8 +45,6 @@ function UserList({users, onRemove, onToggle}){
                     <User 
                         user={user} 
                         key={user.id}
-                        onRemove={onRemove}
-                        onToggle = {onToggle}
                     />
                 ))
             }
